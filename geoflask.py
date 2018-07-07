61ed0d2b48034a7473ce7856a0b8bc7149b994d0
Maxime Rauer <maxime.rauer@gmail.com>
2018-07-07 17:04:43 -0500

    store-as-json

--- geoflask.py
+++ geoflask.py
@@ -1,13 +1,53 @@
+import json
+import os
+
 from osmread import parse_file
 
 
-def process_osm_file():
-    for entity in parse_file('./data/switzerland-latest.osm.pbf'):
+def process_osm_file(osm_file_path):
+    """Process OSM file, converting to a list of dict.
+
+    Keyword arguments:
+    osm_file_path          --  str path of the OSM file
+
+    Returns:
+    entities_list          --  list of dict
+    """
+    entities_list = list()
+    osm_file = parse_file(osm_file_path)
+    for entity in osm_file:
         if(len(entity.tags) > 0 and 'addr:city' in entity.tags
            and 'addr:street' in entity.tags):
+            entities = dict()
+            loc = ','.join([str(entity.lat), str(entity.lon)])
+            text = ' '.join([entity.tags['addr:street'],
+                             entity.tags['addr:city']])
 
-            print ' '.join([entity.tags['addr:street'],
-                            entity.tags['addr:city']])
+            entities['id'] = len(entities_list) + 1
+            entities['fields'] = {}
+            entities['fields']['text'] = text
+            entities['fields']['location_field'] = loc
 
-            print entity.lon
-            print entity.lat
+            print entities
+            entities_list.append(entities)
+        if len(entities_list) == 50000:
+            break
+    return entities_list
+
+
+def write_to_file(entities):
+    """Write to a JSON file"""
+    file = 'data.json'
+    f = open(file, 'w')
+    f.write(json.dumps(entities))
+    f.close()
+    return sizeof_fmt(os.path.getsize(file))
+
+
+def sizeof_fmt(num, suffix='B'):
+    """Convert size of directory in readable units."""
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import geoflask
 
-geoflask.process_osm_file()
+osm_file_path = './data/switzerland-latest.osm.pbf'
+
+entities = geoflask.process_osm_file(osm_file_path)
+
+print geoflask.write_to_file(entities)
