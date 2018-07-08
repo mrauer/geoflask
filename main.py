98898850454a8bd16d1fc77cdb69636deb848338
Maxime Rauer <maxime.rauer@gmail.com>
2018-07-08 15:06:55 -0500

    additional-fields

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import geoflask
 
 osm_file_path = './data/switzerland-latest.osm.pbf'
 
-entities = geoflask.process_osm_file(osm_file_path)
+entities = geoflask.process_osm_file(osm_file_path, 10000)
 
 print geoflask.write_to_file(entities)
--- test_geoflask.py
+++ test_geoflask.py
@@ -1,1 +1,6 @@
-# Test file
+import geoflask
+
+
+def test_sizeof_fmt_mb():
+    """Test size returned is in MB."""
+    assert geoflask.sizeof_fmt(121821489) == '116.2MiB'
